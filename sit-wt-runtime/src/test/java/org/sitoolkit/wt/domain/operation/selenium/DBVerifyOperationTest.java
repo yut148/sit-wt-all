406540889f392fd90473395f97513b1f3a586e6b
kumakuratk <takuya.kumakura@monocrea.jp>
2017-01-17 14:55:24 +0900

    DB比較機能の追加

---
+++ sit-wt-runtime/src/test/java/org/sitoolkit/wt/domain/operation/selenium/DBVerifyOperationTest.java
@@ -0,0 +1,55 @@
+package org.sitoolkit.wt.domain.operation.selenium;
+
+import org.junit.Test;
+import org.sitoolkit.wt.domain.tester.SitTesterTestBase;
+
+/**
+ *
+ */
+public class DBVerifyOperationTest extends SitTesterTestBase {
+
+    @Test
+    public void test001() {
+        test();
+    }
+
+    @Test
+    public void test002() {
+        test();
+    }
+
+    @Test
+    public void test003() {
+        test();
+    }
+
+    @Test
+    public void test004() {
+        test();
+    }
+
+    @Test
+    public void test005() {
+        test();
+    }
+
+    @Test
+    public void test006() {
+        test();
+    }
+
+    @Test
+    public void test007() {
+        test();
+    }
+
+    @Override
+    protected String getTestScriptPath() {
+        return "src/test/resources/selenium/DBVerifyOperationTestScript.xlsx";
+    }
+
+    @Override
+    protected String getSheetName() {
+        return "TestScript";
+    }
+}
---
+++ sit-wt-runtime/tools/db/01-create-experiment.sql
@@ -0,0 +1,6 @@
+/*** DDL ***/
+CREATE TABLE tab1 (
+  ID char(3) PRIMARY KEY,
+  col1 char(3),
+  col2 char(3)
+);
