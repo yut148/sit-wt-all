c9f79d9b91633d2cbec0fb32601c71f3fb198115
kumakuratk <takuya.kumakura@monocrea.jp>
2017-01-20 10:39:27 +0900

    テストクラスでDerbyを起動するように修正

--- sit-wt-runtime/src/test/java/org/sitoolkit/wt/domain/operation/selenium/DBVerifyOperationTest.java
+++ sit-wt-runtime/src/test/java/org/sitoolkit/wt/domain/operation/selenium/DBVerifyOperationTest.java
@@ -1,5 +1,14 @@
 package org.sitoolkit.wt.domain.operation.selenium;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.sitoolkit.wt.domain.tester.SitTesterTestBase;
 
@@ -7,6 +16,29 @@ import org.sitoolkit.wt.domain.tester.SitTesterTestBase;
  *
  */
 public class DBVerifyOperationTest extends SitTesterTestBase {
+
+    @BeforeClass
+    public static void runDerby() {
+        String derbyDir = System.getProperty("basedir") + "/target/derby/testdb";
+        Connection conn = null;
+        Boolean result = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:derby:" + derbyDir + ";create=true");
+            Statement state = conn.createStatement();
+
+            String files[] = { "tools/db/01-create-experiment.sql",
+                    "tools/db/02-insert-experiment.sql" };
+            for (String file : files) {
+                String query = Files.lines(Paths.get(file), Charset.forName("UTF-8"))
+                        .collect(Collectors.joining(System.getProperty("line.separator")));
+                result = state.execute(query.replaceAll(";$", ""));
+            }
+
+        } catch (Exception exp) {
+            exp.printStackTrace();
+        }
+    }
 
     @Test
     public void test001() {
